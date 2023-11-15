1a5f58a1841377b5e4a1b291d5936ffd4248f6ea
TheKhaliqAnsari <khaliqansari771@gmail.com>
2023-11-15 18:14:45 +0530

    Countries api test cases updated: 1. removed before each for each describe and fixed error handling

--- Country flags display assessment/cypress/e2e/home.cy.ts
+++ Country flags display assessment/cypress/e2e/home.cy.ts
@@ -1,60 +1,62 @@
-describe('Countries App', () => {
+describe("Countries App", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
 
-  describe('Loading and Initial Display', () => {
-    beforeEach(() => {
-      cy.visit('/');
-    });
-
-    it('makes an API call and displays initial content', () => {
-      cy.intercept('GET', 'https://restcountries.com/v3.1/all').as('getCountries');
-      cy.wait('@getCountries').its('response.statusCode').should('eq', 200);
-
-      // Check if the app has some content after loading
-      cy.get('body').should('not.be.empty');
+  describe("Loading and Initial Display", () => {
+    it("makes an API call and displays initial content", () => {
+      cy.intercept("GET", "https://restcountries.com/v3.1/all").as(
+        "getCountries"
+      );
+      cy.wait("@getCountries").its("response.statusCode").should("eq", 200);
+      cy.get("body").should("not.be.empty");
     });
   });
 
-  describe('Flag Display', () => {
-    beforeEach(() => {
-      cy.visit('/');
-    });
-
-    it('displays country flags with alt text', () => {
-      cy.get('img').should('have.length.at.least', 250)
+  describe("Flag Display", () => {
+    it("displays country flags with alt text", () => {
+      cy.get("img")
+        .should("have.length.at.least", 250)
         .and(($imgs) => {
-          expect($imgs).to.have.attr('alt').and.not.be.empty;
+          expect($imgs).to.have.attr("alt").and.not.be.empty;
         });
     });
   });
 
-  describe('Country Name Display', () => {
-    beforeEach(() => {
-      cy.visit('/');
-    });
-
-    it('displays country names', () => {
-      // Check for elements likely to contain country names
-      // Ensures these elements are not empty and contain some text
-      cy.get('div, span, h1, h2, h3, h4, h5, h6, p')
+  describe("Country Name Display", () => {
+    it("displays country names", () => {
+      cy.get("div, span, h1, h2, h3, h4, h5, h6, p")
         .filter((index, element) => {
           return element.innerText.trim().length > 0;
         })
-        .should('have.length.at.least', 1);
+        .should("have.length.at.least", 1);
     });
   });
 
-  describe('Error Handling', () => {
-    it('handles API failure gracefully', () => {
-      cy.intercept('GET', 'https://restcountries.com/v3.1/all', {
-        forceNetworkError: true
-      }).as('getFailedCountries');
+  describe("API Error Handling", () => {
+    it("logs an error to the console on API failure", () => {
+      // Create spies on console.log and console.error
+      cy.on("window:before:load", (win) => {
+        cy.spy(win.console, "log").as("consoleLog");
+        cy.spy(win.console, "error").as("consoleError");
+      });
 
-      cy.visit('/');
+      // Intercept the API request and force a network error
+      cy.intercept("GET", "https://restcountries.com/v3.1/all", {
+        forceNetworkError: true,
+      }).as("getFailedCountries");
 
-      // Check for application's error handling
-      // Example:
-      // cy.get('.error-message').should('be.visible').and('contain', 'Error fetching data');
+      cy.visit("/");
+
+      // Wait for the intercepted API call
+      cy.wait("@getFailedCountries");
+
+      // Check if either console.error or console.log was called
+      cy.get("@consoleError").then((consoleError) => {
+        cy.get("@consoleLog").then((consoleLog) => {
+          expect(consoleError.called || consoleLog.called).to.be.true;
+        });
+      });
     });
   });
-
 });
